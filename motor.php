018d5c2f6844b48302ddc87deb581a3da9120c70
Kwahyu09 <krisnawahyudi2017@gmail.com>
2023-02-05 01:29:45 +0700

    menambah method constructer

--- motor.php
+++ motor.php
@@ -3,14 +3,21 @@
 //membuat class
 Class Motor{
     //ini property
-    public $Merk = "Honda",
-        $nama = "Beat",
-        $warna = "Hitam",
-        $harga = 17000000;
+    public $merk, $nama, $warna, $harga ;
 
     //membuat method
     public function Label(){
         return "$this->nama, $this->harga";
+    }
+
+    //membuat method constructur
+    //method yang langsung dijalankan ketika membuat objek
+    public function __construct( $merk="Honda", $nama="Beat", $warna="Hitam", $harga=17000000)
+    {
+        $this->merk = $merk;
+        $this->nama = $nama;
+        $this->warna = $warna;
+        $this->harga = $harga;
     }
 }
 
