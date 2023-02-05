13558306644d4f6125bc4174fccf19042ccd768e
Kwahyu09 <krisnawahyudi2017@gmail.com>
2023-02-05 10:48:27 +0700

    Objek Type

--- motor.php
+++ motor.php
@@ -21,10 +21,22 @@ Class Motor{
     }
 }
 
+
+//Objek Type
+//membuat clas cetak yang dimana selain objek class motor tidak bisa dicetak
+class CetakInfo{
+    public function Cetak(Motor $motor){
+        $str = "{$motor->merk} || {$motor->nama}, Warna : {$motor->warna} Harga : {$motor->harga}";
+        return $str;
+    }
+}
+
 //membuat objek
 $Motor1 = new Motor();
 $Motor1->nama = "Blade";
 $Motor1->harga = 22000000;
-var_dump($Motor1);
 
-echo "motor" . $Motor1->Label();
+echo "motor " . $Motor1->Label();
+echo "<br>";
+$infoCetak = new CetakInfo();
+echo $infoCetak->Cetak($Motor1);
