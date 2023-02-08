a9ce356b022a03d0aa2c49780ec81368646bacc6
Kwahyu09 <krisnawahyudi2017@gmail.com>
2023-02-08 22:40:34 +0700

    Menambahkan file constant dan belajar oop constanr

---
+++ constan.php
@@ -0,0 +1,20 @@
+<?php 
+
+// define('NAMA','Krisna Wahyudi');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 21;
+// echo UMUR;
+
+class Coba{
+    const NAMA = 'Krisna Wahyudi';
+}
+
+
+//Magic Method
+echo __CLASS__;
+echo Coba::NAMA;
+
+?>
